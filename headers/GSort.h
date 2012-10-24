26224fdee86d3821845cf7db7c7da498dba35af3
QSL <q.s.l@live.fr>
2012-10-24 19:02:44 +0200

    doxygen for GSort

--- headers/GSort.h
+++ headers/GSort.h
@@ -8,32 +8,92 @@
 #include "GVCluster.h"
 
 /**
-  * \file Action.h
-  * \brief header for the GSort class
-  * \author PAPPL 2012
+  * @file GSort.h
+  * @brief header for the GSort class
+  * @author PAPPL 2012
   *
   */
 
 class GSort;
 typedef boost::shared_ptr<GSort> GSortPtr;
 
+
+/**
+  * @class Gsort
+  * @brief determines the way to draw a sort
+  *
+  */
 class GSort {
 
 	public:
+
+        /**
+          * @brief constructor for GSort
+          *
+          */
 		GSort(SortPtr p, GVCluster c);
 		~GSort();
-		QGraphicsItem* getDisplayItem (void);		
+
+        /**
+          * @brief contains the whole drawing of the sort
+          *
+          */
+        QGraphicsItem* getDisplayItem (void);
+
+        /**
+          * @brief determines the palette of colours used by the action coming from this sort
+          *
+          */
 		QColor* color;
 		
 	protected:
+
+        /**
+          * @brief contains the whole drawing of the sort
+          *
+          */
 		QGraphicsItem* display;
+
+        /**
+          * @brief graphical element to draw the sort: rectangle
+          *
+          */
 		QGraphicsRectItem* rect;
+
+        /**
+          * @brief graphical element to draw the sort: text
+          *
+          */
 		QGraphicsTextItem* text;
+
+        /**
+          * @brief sort related
+          *
+          */
 		SortPtr sort;
+
+        /**
+          * @brief cluster related
+          *
+          */
 		GVCluster cluster;		
 		
-		static std::vector<QColor> palette; //Drawing colors
+        /**
+          * @brief determines the colours
+          *
+          */
+        static std::vector<QColor> palette;
+
+        /**
+          * @brief palette management index
+          *
+          */
 		static int paletteIndex;
+
+        /**
+          * @brief returns the colour relative to the index
+          *
+          */
 		static QColor* makeColor(void);
 	
 };

9f5ca73a9eb56b89bb60dd7fae262696b477feac
QSL <q.s.l@live.fr>
2012-10-25 14:06:10 +0200

    Modified the doxygen for MainWindow, I didnt documented the menu buttons

--- headers/MainWindow.h
+++ headers/MainWindow.h
@@ -10,22 +10,63 @@
   */
 
 
+/**
+  * @class MainWindow
+  * @brief Builds the main window of the program
+    extends from QMainWindow
+  *
+  */
 class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
+
+    /**
+      * @brief constructor for MainWindow, creates the window, the menus and initializes the characteristics
+      *
+      */
     MainWindow();
     ~MainWindow();
+
+    /**
+      * @brief getter for CentraleArea
+      *
+      * @return a QMdiArea* pointer to the centrale zone of the window
+      *
+      */
     QMdiArea* getCentraleArea();
+
+    /**
+      * @brief gets all the paths of the PH files already opened
+      *
+      */
     std::vector<QString> getAllPaths();
 
     //method for the menu computation
+
+    /**
+      * @brief calls the pint function with the arguments chosen
+      *
+      * @param Qstring the program chosen
+      * @param QStringList the arguments chosen
+      * @param QString the name of the file (optionnal)
+      *
+      */
     void compute(QString program, QStringList arguments, QString fileName="");
 
     //enableMenu (in relation with the slot disableMenu)
+    /**
+      * @brief activates the menus that need a open an active tab
+      *
+      */
     void enableMenu();
 
 protected:
+
+    /**
+      * @brief pointer to the centrale area of the window
+      *
+      */
     QMdiArea* centraleArea;
 
     // QMenu
@@ -68,18 +109,70 @@ signals:
 
 public slots:
     //menu file
+
+    /**
+      * @brief open a new tab
+      *
+      * @return a pointer to a MyArea newly created
+      *
+      */
     MyArea* openTab();
+
+    /**
+      * @brief save the file
+      *
+      */
     void save();
+
+    /**
+      * @brief close the active tab
+      *
+      */
     void closeTab();
 
+    /**
+      * @brief export the actual view to PNG file
+      *
+      */
     void exportPng();
 
     //menu computation
+
+    /**
+      * @brief calls ph-stable functionality of pint
+      *
+      */
     void findFixpoints();
+
+    /**
+      * @brief calls ph-reach functionality of pint
+      *
+      */
     void computeReachability();
+
+    /**
+      * @brief calls ph-exec functionality of pint
+      *
+      */
     void runStochasticSimulation();
+
+    /**
+      * @brief checks the type of the model
+      *
+      */
     void checkModelType();
+
+    /**
+      * @brief calls ph-stat functionality of pint
+      *
+      */
     void statistics();
 
+    /**
+      * @brief permits to disable a menu
+      *
+      * @param QMdiSubWindow* menu you want to disable
+      *
+      */
     void disableMenu(QMdiSubWindow* subWindow);
 };

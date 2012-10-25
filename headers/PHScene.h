66686d13f0a0ecf6138bfef027ae804fe2e4b474
QSL <q.s.l@live.fr>
2012-10-25 19:29:03 +0200

    Created the doxygen for PHScene header

--- headers/PHScene.h
+++ headers/PHScene.h
@@ -32,18 +32,65 @@ typedef boost::shared_ptr<PHScene> PHScenePtr;
 
 typedef std::pair<string, GSortPtr> GSortEntry;
 
+/**
+  * @class PHScene
+  * @brief the graphic object representing the process hitting
+    extends QGraphicsScene
+  *
+  */
 class PHScene: public QGraphicsScene {
 	
 	public:
-		PHScene(PH* _ph);
-		void doRender(void);
+        /**
+          * @brief builder for PHScene
+          *
+          */
+        PHScene(PH* _ph);
+
+        /**
+          * @brief recalculates the position of the scene
+          *
+          */
+        void doRender(void);
+
+        /**
+          * @brief getter for the GSort
+          * @param string: the name of the sort needed
+          * @return GSortPtr: the sort called
+          *
+          */
 		GSortPtr getGSort (const string& s);
 		
 	protected:
+
+        /**
+          * @brief PH file contained
+          *
+          */
 		PH* ph;
+
+        /**
+          * @brief empties the scene and add the elements composing the representation of the process hitting
+          *
+          */
 		void draw(void);
+
+        /**
+          * @brief map of the sorts of the scene
+          *
+          */
 		map<string, GSortPtr> sorts;
+
+        /**
+          * @brief vector of the processes of the scene
+          *
+          */
 		std::vector<GProcessPtr> processes;
+
+        /**
+          * @brief vector of the actions of the scene
+          *
+          */
 		std::vector<GActionPtr> actions;
 	
 };

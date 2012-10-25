6bd9dd98fd53f534180a90ceb399a0194837e698
QSL <q.s.l@live.fr>
2012-10-25 19:15:46 +0200

    Created the doxygen for PHIO header

--- headers/PHIO.h
+++ headers/PHIO.h
@@ -11,16 +11,55 @@
 
 using std::string;
 
+/**
+  * @class PHIO
+  * @brief manages the inputs and outputs of the PH files
+  *
+  */
 class PHIO {
 
 	public:
+        /**
+          * @brief checks if the file exists and is recognized by the parser
+          * @param string the path of the file
+          * @return bool 1 if the file exists
+          *
+          */
 		static bool canParseFile(string const& path);
+
+        /**
+          * @brief parses the file if it is possible
+          * @param string the path of the file
+          * @return PHPtr the PH file parsed
+          *
+          */
 		static PHPtr parseFile  (string const& path);
-		static void writeToFile (string const& path, PHPtr ph);			
+
+        /**
+          * @brief saves the PH object as a PH file
+          * @param string the path of the file
+          * @param PHPtr the object that will be saved
+          *
+          */
+        static void writeToFile (string const& path, PHPtr ph);
+
+        /**
+          * @brief saves as a PNG the representation of the PH file in the window
+          * @param PHPtr the PH object of the active window
+          * @param QString the name of the file saved
+          *
+          */
 		static void exportToPNG (PHPtr ph, QString name);
 	
 	private:
 		PHIO(){}
-		static PHPtr parse (string const& input); 		//Actual parsing
+
+        /**
+          * @brief parses the file coming from PHC
+          * @param string the path of the data
+          * @return PHPtr the PH object parsed
+          *
+          */
+        static PHPtr parse (string const& input);
 
 };

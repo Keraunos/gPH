15c64e03188c350cb7e57f55fa47b157a0dae5a9
QSL <q.s.l@live.fr>
2012-10-25 13:00:34 +0200

    Modified the doxygen for GVCluster

--- headers/GVCluster.h
+++ headers/GVCluster.h
@@ -9,19 +9,34 @@
   *
   */
 
-//A struct containing the information for a GVGraph's node
+/**
+  * @brief struct containing the information for a GVGraph's node
+  *
+  */
 struct GVCluster {
 	
-    //The unique identifier of the cluster in the graph
+    /**
+      * @brief the unique identifier of the cluster in the graph
+      *
+      */
     QString name;
 
-    //The position of the top-left corner
+    /**
+      * @brief the position of the top-left corner
+      *
+      */
     QPoint topLeft;
 
-    //The size of the cluster in pixels
+    /**
+      * @brief the size (height and width) of the cluster in pixels
+      *
+      */
     float height, width;
 	
-	//The label position
+    /**
+      * @brief the label position
+      *
+      */
 	QPoint labelPos;
 	
 };

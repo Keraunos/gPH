129c51d8fd285f40ed2460e3658ddbd1bd1de471
QSL <q.s.l@live.fr>
2012-10-26 08:25:52 +0200

    Finished the modifications of the doxygen base

--- documentation/html/search/all_6d.js
+++ documentation/html/search/all_6d.js
@@ -5,12 +5,6 @@ var searchData=
   ['make',['make',['../class_sort.html#a9c3fa1c3b71839c425d99a7fa86a9cc2',1,'Sort']]],
   ['makearrowhead',['makeArrowHead',['../class_g_action.html#afeb408bf85d902191cef8a3cf613c7eb',1,'GAction']]],
   ['makecolor',['makeColor',['../class_g_sort.html#a03da0205abd58bad6c4a58419cb40568',1,'GSort']]],
-  ['menucomputation',['menuComputation',['../class_main_window.html#a8142152915924723cee2f22e0868a852',1,'MainWindow']]],
-  ['menuedit',['menuEdit',['../class_main_window.html#a51ec7fcfcb60073b395dee46daacf1f9',1,'MainWindow']]],
-  ['menuexport',['menuExport',['../class_main_window.html#a509ebce4344493751dec180d55d3b107',1,'MainWindow']]],
-  ['menufile',['menuFile',['../class_main_window.html#a2ca04227e7d71b036ccd0ed4176a5561',1,'MainWindow']]],
-  ['menuhelp',['menuHelp',['../class_main_window.html#a81d80bba8e8a31cea2bb218094890c81',1,'MainWindow']]],
-  ['menuview',['menuView',['../class_main_window.html#a57793b17cc2b8de42b5b10e9458ae9cb',1,'MainWindow']]],
   ['myarea',['MyArea',['../class_my_area.html',1,'MyArea'],['../class_my_area.html#a31f13e95c83414c538b6c46e55e19c79',1,'MyArea::MyArea()']]],
   ['myarea_2eh',['MyArea.h',['../_my_area_8h.html',1,'']]],
   ['myphptr',['myPHPtr',['../class_my_area.html#a59beb02fefc60d81c287cbc43e6de640',1,'MyArea']]]

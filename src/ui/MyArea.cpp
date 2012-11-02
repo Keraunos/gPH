7755e92e3a549d93bd09d992c830702878041a2e
Keraunos <gaetan.girin.eu@gmail.com>
2012-11-02 17:02:14 +0100

    Added comments in MyArea class

--- src/ui/MyArea.cpp
+++ src/ui/MyArea.cpp
@@ -1,25 +1,36 @@
 #include <QPainter>
 #include "MyArea.h"
 
+
+/**
+  * @class MyArea
+  * @brief class instantiated for each new tab
+  */
+
+
+// constructor
 MyArea::MyArea(QString path) : QGraphicsView() {
     this->path = path;
 	setRenderHints (QPainter::Antialiasing);
 }
 
+
+// get/set myPHPtr
 PHPtr MyArea::getPHPtr() {
     return this->myPHPtr;
 }
-
 void MyArea::setPHPtr(PHPtr pPHPtr) {
      this->myPHPtr = pPHPtr;
 }
 
+
+// get/set path
 QString MyArea::getPath() {
     return this->path;
 }
-
 void MyArea::setPath(QString Path) {
      this->path = path;
 }
+
 
 #include "moc_MyArea.cpp"

2b190b4273c88ac63c74cc02f5a1c8f27e458ea5
Keraunos <gaetan.girin.eu@gmail.com>
2012-11-07 23:40:33 +0100

    Added comments for doc in GSortItem class

--- headers/GSortItem.h
+++ headers/GSortItem.h
@@ -1,15 +1,47 @@
 #pragma once
 #include <QGraphicsItemGroup>
 
+/**
+  * @file GSortItem.h
+  * @brief GSortItem class header file
+  * @author PGROU_2012
+  *
+  */
+
 class GSort;
 
+
+/**
+  * @class GSortItem
+  * @brief graphic item representing a Sort
+  *
+  */
 class GSortItem : public QGraphicsItemGroup {
 
     public:
+
+        /**
+          * @brief Constructor
+          *
+          * @param GSort the related graphic sort object
+          */
         GSortItem(GSort *sort);
+
         ~GSortItem();
 
+        /**
+          * @brief Handles mouse press event
+          *
+          * @param QGraphicsSceneMouseEvent the event to be handled
+          */
         void mousePressEvent(QGraphicsSceneMouseEvent *event);
+
+        /**
+          * @brief Handles mouse release event
+          *
+          * @param QGraphicsSceneMouseEvent the event to be handled
+          */
+        //void mouseReleaseEvent(QGraphicsSceneMouseEvent *event);
 
     private:
         GSort* sort;
--- src/gfx/GSortItem.cpp
+++ src/gfx/GSortItem.cpp
@@ -11,13 +11,21 @@ GSortItem::~GSortItem() {}
 
 void GSortItem::mousePressEvent(QGraphicsSceneMouseEvent *event) {
     QCursor *cursor = new QCursor(Qt::ClosedHandCursor);
-    setCursor(*cursor);
+    sort->getRect()->setCursor(*cursor);
 //    // text
-//    qDebug() << "x = " << childItems()[1]->x() << " - y = " << childItems()[1]->y();
-//    // rect
-//    qDebug() << "x = " << sort->getRect()->x() << " - y = " << sort->getRect()->y();
+//    qDebug() << "x = " << childItems()[1]->x() << " | y = " << childItems()[1]->y();
+//    //rect
+//    qDebug() << "x = " << sort->getRect()->x() << " | y = " << sort->getRect()->y();
 //    // cluster
-//    qDebug() << "x = " << sort->getCluster().topLeft.x() << " - y = " << sort->getCluster().topLeft.y();
-    //setTransform(QTransform().translate(x(), y()).rotate(90.0).translate(-x(), -y()));
+//    qDebug() << "x = " << sort->getCluster().topLeft.x() << " | y = " << sort->getCluster().topLeft.y();
+//    qreal _x = sort->getCluster().topLeft.x();
+//    qreal _y = sort->getCluster().topLeft.y();
+//    setTransform(QTransform().translate(_x, _y).rotate(90.0).translate(-_x, -_y));
     event->accept();
 }
+
+//void GSortItem::mouseReleaseEvent(QGraphicsSceneMouseEvent *event) {
+//    QCursor *cursor = new QCursor(Qt::OpenHandCursor);
+//    sort->getRect()->setCursor(*cursor);
+//    event->accept();
+//}
